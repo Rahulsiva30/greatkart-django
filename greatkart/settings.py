17fe9435851c71f9fb81678a68d9a39ac74ef103
rahul <rahulsiv3032000@gmail.com>
2024-10-07 19:11:50 +0530

    handled cart and checkout page for the logged in users

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,3 +138,18 @@ MEDIA_ROOT = BASE_DIR /'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
+
+
+#SMTP configuration
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'rahulsiv3032000@gmail.com'
+# EMAIL_HOST_PASSWORD = 'wnksrrrcbfpxuwua'
+# EMAIL_USER_TLS = True
+# EMAIL_USER_SSL = False
