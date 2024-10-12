8ac632fa11377593f49626b89db6489efe60e95f
rahul <rahulsiv3032000@gmail.com>
2024-10-12 19:11:06 +0530

    review and rating system

--- store/views.py
+++ store/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product
 from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db.models import Q
+from store.models import ReviewRating
+from .forms import ReviewForm
+from django.contrib import messages
+from orders.models import Orderproduct
 
 # Create your views here.
 def store(request, category_slug=None):
@@ -38,10 +42,21 @@ def product_detail(request, category_slug, product_slug):
         in_cart = CartItem.objects.filter(cart__cart_id= _cart_id(request), product= single_product).exists()
     except Exception as e:
         raise e
+    if request.user.is_authenticated:
+        try:
+            orderproduct = Orderproduct.objects.filter(user=request.user, product_id=single_product.id).exists()
+        except Orderproduct.DoesNotExist:
+            orderproduct = None
+    else:
+        orderproduct = None
     
+    reviews = ReviewRating.objects.filter(product_id = single_product.id, status=True)
+
     context = {
         'single_product': single_product,
         'in_cart': in_cart,
+        'orderproduct': orderproduct,
+        'reviews': reviews,
     }
     return render(request, "store/product_detail.html", context)
 
@@ -56,3 +71,53 @@ def search(request):
         'product_count': product_count,
     }
     return render(request, "store/store.html", context)
+
+# def submit_review(request, product_id):
+#     url = request.META.get('HTTP_REFERER')
+#     if request.method == "POST":
+#         try:
+#             reviews = ReviewRating.objects.get(user__id=request.user.id, product__id=product_id)
+#             form = ReviewForm(request.POST, instance=reviews)
+#             form.save()
+#             messages.success(request, 'Thank you! your review has been submited')
+#             return redirect(url)
+        
+#         except ReviewRating.DoesNotExist:
+#             form = ReviewForm(request.POST)
+#             if form.is_valid():
+#                 data = ReviewRating()
+#                 data.subject = form.cleaned_data['subject']
+#                 data.rating = form.cleaned_data['rating']
+#                 data.review = form.cleaned_data['review']
+#                 data.ip = request.META.get('REMOTE_ADDR')
+#                 data.product_id = product_id
+#                 data.user_id = request.user.id
+#                 data.save()
+#                 messages.success(request, 'Thank you! your rating has been submited')
+#                 return redirect(url)
+            
+def submit_review(request, product_id):
+    url = request.META.get('HTTP_REFERER')  # Get the referrer URL
+    if request.method == "POST":
+        try:
+            # Attempt to get the existing review for the user and product
+            reviews = ReviewRating.objects.get(user__id=request.user.id, product__id=product_id)
+            form = ReviewForm(request.POST, instance=reviews)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Thank you! Your review has been submitted.')
+                return redirect(url)  
+            
+        except ReviewRating.DoesNotExist:
+            form = ReviewForm(request.POST)
+            if form.is_valid():
+                data = ReviewRating()
+                data.subject = form.cleaned_data['subject']
+                data.rating = form.cleaned_data['rating']
+                data.review = form.cleaned_data['review']
+                data.ip = request.META.get('REMOTE_ADDR')
+                data.product_id = product_id
+                data.user_id = request.user.id
+                data.save()
+                messages.success(request, 'Thank you! Your rating has been submitted.')
+                return redirect(url) 
