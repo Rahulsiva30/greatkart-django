13d999237fd6bd5e128c9cb94ca6183cc3b93d1b
rahul <rahulsiv3032000@gmail.com>
2025-08-19 11:19:33 +0530

    Fix home view: attach reviews and averageReview to products

--- greatkart/views.py
+++ greatkart/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from store.models import Product, ReviewRating
 
-def home(request):
-    products = Product.objects.all().filter(is_available=True).order_by('created_date')
-    
-    for product in products:
-        reviews = ReviewRating.objects.filter(product_id = product.id, status=True)
 
+def home(request):
+    products = Product.objects.filter(is_available=True).order_by("created_date")
+
+    for product in products:
+        # Get approved reviews for each product
+        reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
+        product.reviews = reviews  # attach reviews to each product
+
+        # If your Product model has a method averageReview(), call it here
+        product.averageReview = product.averageReview()
 
     context = {
-        'products': products,
-        'reviews': reviews,
+        "products": products,
     }
-
-    return render(request, 'home.html', context)
+    return render(request, "home.html", context)
